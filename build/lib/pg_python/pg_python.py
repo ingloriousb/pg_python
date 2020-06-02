b802c0597b063e13bb30fadae491e419ef86b22b
przdev <prashantgodhani7@gmail.com>
2020-06-02 14:37:49 +0530

    added all fixes to build/lib

--- build/lib/pg_python/pg_python.py
+++ build/lib/pg_python/pg_python.py
@@ -7,25 +7,71 @@ from ._delete import make_postgres_delete_statement
 from ._update import make_postgres_update_multiple_column_statement
 from ._update import check_parameters_multicol
 import logging
+import signal
 
 db_dict = {}
+
+
+class timeout:
+    """Need because of sometimes query will take very long
+    (cause of connection already killed and tring to execute query)"""
+
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
 
 def get_db(server="default"):
     db_obj = db_dict.get(server, None)
     return db_obj
 
+
 print_debug_log = True
 
 
-def pg_server(db_name, username, password, host_address, debug=True, server="default", send_keep_alive_probes=False, socket_idle_time=120):
+def server_connection_check(func):
+    def wrapper(*args, **kwargs):
+        server = kwargs.get('server', 'default')
+        db_obj = get_db(server)
+        if db_obj.connection.closed != 0:
+            logging.info('reconnection to db because of connection closed %s' % db_obj.connection.closed)
+            db_obj._make_connection()
+        else:
+            cursor = db_obj.get_cursor()
+            try:
+                with timeout(seconds=10):
+                    cursor.execute('SELECT 1', [])
+            except TimeoutError as e:
+                logging.info('timeout occurred, reconnecting dbs')
+                db_obj._make_connection()
+            except Exception as e:
+                logging.info('reconnection to db because of %s' % e)
+                db_obj._make_connection()
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def pg_server(db_name, username, password, host_address, debug=True, server="default", send_keep_alive_probes=False,
+              socket_idle_time=120, application_name='pg_python'):
     global print_debug_log
+    # no need to make socket so, send_keep_alive_probes will no longer usefull
     params_map = {
-        'dbname'  : db_name,
-        'user'    : username,
+        'dbname': db_name,
+        'user': username,
         'password': password,
-        'host'    : host_address,
-        'send_keep_alive_probes': send_keep_alive_probes,
-        'socket_idle_time': socket_idle_time,
+        'host': host_address,
+        'application_name': application_name
     }
     db_obj = Db(params_map)
     db_dict[server] = db_obj
@@ -33,6 +79,7 @@ def pg_server(db_name, username, password, host_address, debug=True, server="def
     return db_obj
 
 
+@server_connection_check
 def write(table, kv_map, server="default"):
     """
     :param table: String.
@@ -58,6 +105,8 @@ def write(table, kv_map, server="default"):
         return False
     return True
 
+
+@server_connection_check
 def read(table, keys_to_get, kv_map, limit=None, order_by=None, order_type=None, clause="=", group_by=None,
          join_clause=' AND ', server="default"):
     """
@@ -84,6 +133,8 @@ def read(table, keys_to_get, kv_map, limit=None, order_by=None, order_type=None,
         logging.warning("Db Cursor Read Error: %s" % e)
         return []
 
+
+@server_connection_check
 def update(table, update_kv_map, where_kv_map, clause='=', server="default"):
     """
     :param table: table name, type string
@@ -110,6 +161,8 @@ def update(table, update_kv_map, where_kv_map, clause='=', server="default"):
         return False
     return return_dict
 
+
+@server_connection_check
 def read_raw(command, values, server="default"):
     """
     :param table: String
@@ -133,6 +186,8 @@ def read_raw(command, values, server="default"):
         logging.warning("Db Cursor Read Error: %s" % e)
         return []
 
+
+@server_connection_check
 def write_raw(command, values, server="default"):
     """
     :params command, values. Execution commands dirctly for postgres
@@ -153,6 +208,7 @@ def write_raw(command, values, server="default"):
     return True
 
 
+@server_connection_check
 def update_raw(command, server="default"):
     """
     Update statement in the raw format,
@@ -175,6 +231,7 @@ def update_raw(command, server="default"):
         return -1
     return rowcount
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -185,17 +242,21 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def print_ok(s):
     print((bcolors.OKGREEN + s + bcolors.ENDC))
+
 
 def print_warn(s):
     print((bcolors.WARNING + s + bcolors.ENDC))
 
+
 def print_fail(s):
     print((bcolors.FAIL + s + bcolors.ENDC))
 
+
 def close(server="default"):
-    logging.info("Closing connection for server %s" %server)
+    logging.info("Closing connection for server %s" % server)
     db_obj = get_db(server)
     connection = db_obj.get_connection()
     try:
@@ -211,6 +272,7 @@ def close_all():
         close(server)
 
 
+@server_connection_check
 def delete(table, where_kv_map, server="default"):
     """
     Delete the rows resulting from the mentined kv map. No limit.
@@ -225,7 +287,7 @@ def delete(table, where_kv_map, server="default"):
     command, values = make_postgres_delete_statement(table, where_kv_map, print_debug_log)
     try:
         cursor.execute(command, values)
-        logging.info("%s Record(s) Deleted" %cursor.rowcount)
+        logging.info("%s Record(s) Deleted" % cursor.rowcount)
         connection.commit()
     except Exception as e:
         logging.warning("Db Cursor Delete Error: %s" % e)
@@ -235,6 +297,8 @@ def delete(table, where_kv_map, server="default"):
         return False
     return True
 
+
+@server_connection_check
 def check_parameters(column_to_update, columns_to_query_lst, query_values_dict_lst):
     """
     check_prarameters checks whether the passed parameters are valid or not.
@@ -260,8 +324,10 @@ def check_parameters(column_to_update, columns_to_query_lst, query_values_dict_l
                 return False
     return True
 
+
+@server_connection_check
 def update_multiple(table, column_to_update, columns_to_query_lst,
-                    query_values_dict_lst, server="default", typecast = ""):
+                    query_values_dict_lst, server="default", typecast=""):
     """
     Multiple update support in pg_python
     :param table: table to update into
@@ -303,6 +369,8 @@ def update_multiple(table, column_to_update, columns_to_query_lst,
         return {'status': False}
     return return_dict
 
+
+@server_connection_check
 def update_multiple_col(table, columns_to_update_lst, columns_to_query_lst, query_values_dict_lst,
                         server="default"):
     """
@@ -326,11 +394,11 @@ def update_multiple_col(table, columns_to_update_lst, columns_to_query_lst, quer
         logging.error("ERROR in parameters passed")
         return {'status': False}
 
-    command, values =  make_postgres_update_multiple_column_statement(table,
-                                                                      columns_to_update_lst,
-                                                                      columns_to_query_lst,
-                                                                      query_values_dict_lst,
-                                                                      print_debug_log)
+    command, values = make_postgres_update_multiple_column_statement(table,
+                                                                     columns_to_update_lst,
+                                                                     columns_to_query_lst,
+                                                                     query_values_dict_lst,
+                                                                     print_debug_log)
     try:
         cursor.execute(command, values)
         count = cursor.rowcount
@@ -341,8 +409,10 @@ def update_multiple_col(table, columns_to_update_lst, columns_to_query_lst, quer
         db_dict.pop(server)
         db_dict[server] = db_obj_reconnect
         return {'status': False}
-    return {'status':True, 'updated_records':count}
+    return {'status': True, 'updated_records': count}
 
+
+@server_connection_check
 def check_multiple_insert_param(columns_to_insert, insert_values_dict_lst):
     """
     Checks if the pararmeter passed are of correct order.
@@ -361,6 +431,8 @@ def check_multiple_insert_param(columns_to_insert, insert_values_dict_lst):
                 return False
     return True
 
+
+@server_connection_check
 def insert_multiple(table, columns_to_insert_lst, insert_values_dict_lst, server="default"):
     """
     Multiple row insert in pg_python
